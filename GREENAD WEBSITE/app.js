bc096046faed4faa2e97a8daf8e2207d3a6a267e
swapna sikdar <126584308+Swapna71@users.noreply.github.com>
2024-02-13 19:58:07 +0530

    HTML, CSS, JAVASCRIPT

---
+++ GREENAD WEBSITE/app.js
@@ -0,0 +1,17 @@
+
+
+rtc =()=>{
+    Array.from(document.getElementsByClassName("wi-ch")).forEach((el)=>{
+        el.style.width=0
+    })
+}
+
+Array.from(document.getElementsByClassName("w-ch")).forEach((el, i)=>{
+el.addEventListener("click", ()=>{
+  rtc();
+  let rnd=Math.floor(Math.random()*360)
+  document.getElementsByClassName("wi-ch")[i].style.width="200px";
+  document.getElementById(("val")).innerText='0'+(i +1);
+  document.getElementsByTagName("header")[0].style.filter=`hue-rotate(${rnd}deg)`
+})
+})
